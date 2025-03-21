8604fc546ad1e3ae4ffbe1c7a3b9f4bb4bd22369
Minecraftpro546 <111045496+Minecraftpro546@users.noreply.github.com>
2025-03-20 20:35:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,44 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener("click", mobileMenu);
+
+function calculateEstimate() {
+  const size = document.getElementById("size").value;
+  const tires = document.getElementById("tires").value;
+  const flavor = document.getElementById("flavor").value;
+  const decorations = document.getElementById("decorations").checked;
+
+  let basePrice = 0;
+
+  switch (size) {
+    case "6":
+      basePrice = 20;
+      break;
+    case "8":
+      basePrice = 30;
+      break;
+    case "10":
+      basePrice = 40;
+      break;
+    case "12":
+      basePrice = 50;
+      break;
+  }
+
+  switch (tires) {
+    case "1":
+      basePrice += 50;
+      break;
+    case "2":
+      basePrice += 100;
+      break;
+  }
+
+  if (decorations) {
+    basePrice += 10;
+  }
+
+  document.getElementById(
+    "estimate"
+  ).innerText = `Estimated Price: $${basePrice}`;
+}
